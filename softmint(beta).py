2ccfab48ecd047aea65110476d5ebf03d0c8f7f7
coder09lop <118464452+coder09lop@users.noreply.github.com>
2024-04-07 00:13:50 +0200

    Add files via upload

---
+++ softmint(beta).py
@@ -0,0 +1,17 @@
+print("ciao benvenuto nell sistema centrale HackerBan")
+print("come ti chiami?")
+nome_anonimo = input()
+print("quanti anni hai? UTENTEANONIMUS231767")
+ilinput = input()
+if ilinput < '18':
+    print("mi spiace non possiamo farti entrare")
+else:
+    print("entri pure")
+if ilinput > '18':
+    print("ok  cosa vuole fare?"
+          "1 esci 2 login 3 h4ck.exe")
+reputazion_anonima = input()
+if reputazion_anonima != 'h4ck.exe':
+    print("ok ora sta hackerando la NASA")
+    print("come si sente?")
+sensazione = input()
